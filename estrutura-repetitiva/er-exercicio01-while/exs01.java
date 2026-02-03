ba72827136fdf1de7aa3d18b2f959b51d22c9e2a
brancog7 <gabrielbrancoslv@gmail.com>
2026-02-02 22:53:37 -0300

    exercicio 02 - estrutura repetitiva while

---
+++ estrutura-repetitiva/er-exercicio01-while/exs01.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class exs01 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		
+		System.out.print("Digite a senha: ");
+		int senha = sc.nextInt();
+		
+			while (senha != 2002) {
+				System.out.println("Senha inválida!");
+				System.out.print("Digite novamente: ");
+				senha = sc.nextInt();
+			}
+		
+		System.out.println(senha);
+		System.out.print("Acesso permitido!");
+		
+		sc.close();
+	}
+
+}
